81079f8c631c18eb39ed1c2e08262de43762b89f
HomerIce <wyh900207@126.com>
2019-04-18 17:13:54 +0800

    Initial Commit

---
+++ StockLineDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StockLineDemo
+//
+//  Created by wyh on 2019/4/18.
+//  Copyright © 2019 wyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StockLineDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StockLineDemo
+//
+//  Created by wyh on 2019/4/18.
+//  Copyright © 2019 wyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
